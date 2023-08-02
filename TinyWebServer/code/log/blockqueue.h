31aeea55dd1a94b5d7ea20ae7792b21854a9931c
crossoverpptx <2269041628@qq.com>
2023-08-02 19:23:48 +0800

    2023-08-02 19:23

--- TinyWebServer/code/log/blockqueue.h
+++ TinyWebServer/code/log/blockqueue.h
@@ -1,119 +1,129 @@
 # ifndef BLOCKQUEUE_H
 # define BLOCKQUEUE_H
 
-#include <deque>
-#include <condition_variable>
-#include <mutex>
-#include <sys/time.h>
+#include <deque> // 双端队列容器
+#include <condition_variable> // 条件变量：C++11标准同步原语，它可以在同一时间阻塞一个线程或者多个线程，直到其他线程改变了共享变量（条件）并通知。
+#include <mutex> // 在使用条件变量时，通常需要与互斥锁（mutex）结合使用。
+#include <sys/time.h> // Linux系统的日期时间头文件
+
 using namespace std;
 
 template<typename T>
-class BlockQueue {
+class BlockQueue { // 阻塞队列
 public:
-    explicit BlockQueue(size_t maxsize = 1000);
+    explicit BlockQueue(size_t maxsize = 1000); // explicit修饰的构造函数必须被显式调用
     ~BlockQueue();
+    void clear();
+    void Close();
     bool empty();
     bool full();
     void push_back(const T& item);
     void push_front(const T& item); 
     bool pop(T& item);  // 弹出的任务放入item
     bool pop(T& item, int timeout);  // 等待时间
-    void clear();
     T front();
     T back();
     size_t capacity();
     size_t size();
-
     void flush();
-    void Close();
 
 private:
     deque<T> deq_;                      // 底层数据结构
-    mutex mtx_;                         // 锁
+    mutex mtx_;                         // 锁：互斥锁
     bool isClose_;                      // 关闭标志
     size_t capacity_;                   // 容量
     condition_variable condConsumer_;   // 消费者条件变量
     condition_variable condProducer_;   // 生产者条件变量
 };
 
+// 构造函数：阻塞队列初始化
 template<typename T>
 BlockQueue<T>::BlockQueue(size_t maxsize) : capacity_(maxsize) {
     assert(maxsize > 0);
     isClose_ = false;
 }
 
+// 析构函数：阻塞队列关闭
 template<typename T>
 BlockQueue<T>::~BlockQueue() {
     Close();
 }
 
-template<typename T>
-void BlockQueue<T>::Close() {
-    // lock_guard<mutex> locker(mtx_); // 操控队列之前，都需要上锁
-    // deq_.clear();                   // 清空队列
-    clear();
-    isClose_ = true;
-    condConsumer_.notify_all();
-    condProducer_.notify_all();
-}
-
+// 清空队列
 template<typename T>
 void BlockQueue<T>::clear() {
     lock_guard<mutex> locker(mtx_);
     deq_.clear();
 }
 
+// 关闭队列
+template<typename T>
+void BlockQueue<T>::Close() {
+    // lock_guard<mutex> locker(mtx_); // 操控队列之前，都需要上锁
+    // deq_.clear();                   // 清空队列
+    clear();
+    isClose_ = true;
+    condConsumer_.notify_all(); // notify_all :将所有的线程中的wait都被唤醒。
+    condProducer_.notify_all(); // 虽然是将所有处于睡眠的wait()唤醒，但还是只能由一个线程拿到互斥锁，所以看起来的效果与notify_one相同，但可以用于不同的工作场景。
+}
+
+// 判断队列是否为空
 template<typename T>
 bool BlockQueue<T>::empty() {
     lock_guard<mutex> locker(mtx_);
     return deq_.empty();
 }
 
+// 判断队列是否已满
 template<typename T>
 bool BlockQueue<T>::full() {
     lock_guard<mutex> locker(mtx_);
     return deq_.size() >= capacity_;
 }
 
+// 向队列末尾添加一个元素
 template<typename T>
 void BlockQueue<T>::push_back(const T& item) {
-    // 注意，条件变量需要搭配unique_lock
+    // 注意，条件变量需要搭配unique_lock。unique_lock比lock_guard灵活很多（多出来很多用法），效率差一点，内存占用多一点。
     unique_lock<mutex> locker(mtx_);    
     while(deq_.size() >= capacity_) {   // 队列满了，需要等待
         condProducer_.wait(locker);     // 暂停生产，等待消费者唤醒生产条件变量
     }
-    deq_.push_back(item);
+    deq_.push_back(item);               // 队列有空，向末尾添加一个元素
     condConsumer_.notify_one();         // 唤醒消费者
 }
 
+// 向队列开头添加一个元素
 template<typename T>
 void BlockQueue<T>::push_front(const T& item) {
     unique_lock<mutex> locker(mtx_);
     while(deq_.size() >= capacity_) {   // 队列满了，需要等待
         condProducer_.wait(locker);     // 暂停生产，等待消费者唤醒生产条件变量
     }
-    deq_.push_front(item);
+    deq_.push_front(item);              // 队列有空，向开头添加一个元素
     condConsumer_.notify_one();         // 唤醒消费者
 }
 
+// 删除队首元素
 template<typename T>
 bool BlockQueue<T>::pop(T& item) {
     unique_lock<mutex> locker(mtx_);
     while(deq_.empty()) {
         condConsumer_.wait(locker);     // 队列空了，需要等待
     }
-    item = deq_.front();
-    deq_.pop_front();
+    item = deq_.front();                // 获取队首元素
+    deq_.pop_front();                   // 删除队首元素
     condProducer_.notify_one();         // 唤醒生产者
     return true;
 }
 
+// 带计时器的pop函数
 template<typename T>
 bool BlockQueue<T>::pop(T &item, int timeout) {
     unique_lock<std::mutex> locker(mtx_);
     while(deq_.empty()){
-        if(condConsumer_.wait_for(locker, std::chrono::seconds(timeout)) 
-                == std::cv_status::timeout){
+        if(condConsumer_.wait_for(locker, std::chrono::seconds(timeout))
+                == std::cv_status::timeout){ // 阻塞时间超过指定的timeout，直接返回false
             return false;
         }
         if(isClose_){
@@ -126,24 +136,28 @@ bool BlockQueue<T>::pop(T &item, int timeout) {
     return true;
 }
 
+// 返回队首元素
 template<typename T>
 T BlockQueue<T>::front() {
     lock_guard<std::mutex> locker(mtx_);
     return deq_.front();
 }
 
+// 返回队尾元素
 template<typename T>
 T BlockQueue<T>::back() {
     lock_guard<std::mutex> locker(mtx_);
     return deq_.back();
 }
 
+// 返回队列容量
 template<typename T>
 size_t BlockQueue<T>::capacity() {
     lock_guard<std::mutex> locker(mtx_);
     return capacity_;
 }
 
+// 返回队列当前大小
 template<typename T>
 size_t BlockQueue<T>::size() {
     lock_guard<std::mutex> locker(mtx_);
@@ -155,4 +169,5 @@ template<typename T>
 void BlockQueue<T>::flush() {
     condConsumer_.notify_one();
 }
+
 # endif
--- TinyWebServer/code/log/log.cpp
+++ TinyWebServer/code/log/log.cpp
@@ -15,7 +15,7 @@ Log::~Log() {
         deque_->flush();    // 唤醒消费者，处理掉剩下的任务
     }
     deque_->Close();    // 关闭队列
-    writeThread_->join();   // 等待当前线程完成手中的任务
+    writeThread_->join();   // 阻塞，等待当前线程完成手中的任务
     if(fp_) {       // 冲洗文件缓冲区，关闭文件描述符
         lock_guard<mutex> locker(mtx_);
         flush();        // 清空缓冲区中的数据
@@ -31,7 +31,7 @@ void Log::flush() {
     fflush(fp_);    // 清空输入缓冲区
 }
 
-// 懒汉模式 局部静态变量法（这种方法不需要加锁和解锁操作）
+// 懒汉模式：局部静态变量法（这种方法不需要加锁和解锁操作）
 Log* Log::Instance() {
     static Log log;
     return &log;
@@ -47,7 +47,8 @@ void Log::AsyncWrite_() {
     string str = "";
     while(deque_->pop(str)) {
         lock_guard<mutex> locker(mtx_);
-        fputs(str.c_str(), fp_);
+        fputs(str.c_str(), fp_); // C语言库函数，把字符串写入到指定的流(stream)中，但不包括空字符，不自动写入字符串结束标记符‘\0’。
+                                 // 成功写入一个字符串后，文件的位置指针会自动后移，函数返回值为非负整数；否则返回EOF(符号常量，其值为-1)。
     }
 }
 
@@ -63,7 +64,7 @@ void Log::init(int level, const char* path, const char* suffix, int maxQueCapaci
             unique_ptr<BlockQueue<std::string>> newQue(new BlockQueue<std::string>);
             // 因为unique_ptr不支持普通的拷贝或赋值操作,所以采用move
             // 将动态申请的内存权给deque，newDeque被释放
-            deque_ = move(newQue);  // 左值变右值,掏空newDeque
+            deque_ = move(newQue);  // 左值变右值，掏空newDeque
 
             unique_ptr<thread> newThread(new thread(FlushLogThread));
             writeThread_ = move(newThread);
@@ -100,22 +101,23 @@ void Log::init(int level, const char* path, const char* suffix, int maxQueCapaci
     }
 }
 
+// 写日志：将输出内容按照标准格式整理
 void Log::write(int level, const char *format, ...) {
-    struct timeval now = {0, 0};
-    gettimeofday(&now, nullptr);
-    time_t tSec = now.tv_sec;
+    struct timeval now = {0, 0};    // 用于表示时间的结构体，通常在操作系统编程中使用。它包含两个字段，分别是秒数和微秒数。
+    gettimeofday(&now, nullptr);    // 获取时间，精度可以达到微妙
+    time_t tSec = now.tv_sec;       // 获取系统当前时间的秒数
     struct tm *sysTime = localtime(&tSec);
     struct tm t = *sysTime;
-    va_list vaList;
+    va_list vaList; // 可变参数
 
-    // 日志日期 日志行数  如果不是今天或行数超了
+    // 日志日期 || 日志行数：如果不是今天或行数超了
     if (toDay_ != t.tm_mday || (lineCount_ && (lineCount_  %  MAX_LINES == 0)))
     {
         unique_lock<mutex> locker(mtx_);
         locker.unlock();
         
         char newFile[LOG_NAME_LEN];
-        char tail[36] = {0};
+        char tail[36] = {0};    // 日志每行起始部分36个字符：日期、等级
         snprintf(tail, 36, "%04d_%02d_%02d", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday);
 
         if (toDay_ != t.tm_mday)    // 时间不匹配，则替换为最新的日志文件名
@@ -123,8 +125,7 @@ void Log::write(int level, const char *format, ...) {
             snprintf(newFile, LOG_NAME_LEN - 72, "%s/%s%s", path_, tail, suffix_);
             toDay_ = t.tm_mday;
             lineCount_ = 0;
-        }
-        else {
+        } else {
             snprintf(newFile, LOG_NAME_LEN - 72, "%s/%s-%d%s", path_, tail, (lineCount_  / MAX_LINES), suffix_);
         }
         
@@ -147,7 +148,7 @@ void Log::write(int level, const char *format, ...) {
         AppendLogLevelTitle_(level);    
 
         va_start(vaList, format);
-        int m = vsnprintf(buff_.BeginWrite(), buff_.WritableBytes(), format, vaList);
+        int m = vsnprintf(buff_.BeginWrite(), buff_.WritableBytes(), format, vaList); // 将格式化数据从可变参数列表写入缓冲区
         va_end(vaList);
 
         buff_.HasWritten(m);
@@ -155,8 +156,8 @@ void Log::write(int level, const char *format, ...) {
 
         if(isAsync_ && deque_ && !deque_->full()) { // 异步方式（加入阻塞队列中，等待写线程读取日志信息）
             deque_->push_back(buff_.RetrieveAllToStr());
-        } else {    // 同步方式（直接向文件中写入日志信息）
-            fputs(buff_.Peek(), fp_);   // 同步就直接写入文件
+        } else {                                    // 同步方式（直接向文件中写入日志信息）
+            fputs(buff_.Peek(), fp_);               // 同步就直接写入文件
         }
         buff_.RetrieveAll();    // 清空buff
     }
@@ -183,11 +184,13 @@ void Log::AppendLogLevelTitle_(int level) {
     }
 }
 
+// 获取日志等级
 int Log::GetLevel() {
     lock_guard<mutex> locker(mtx_);
     return level_;
 }
 
+// 设置日志等级
 void Log::SetLevel(int level) {
     lock_guard<mutex> locker(mtx_);
     level_ = level;
